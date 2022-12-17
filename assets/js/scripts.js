da5e2c4bf7043bedb05090e6d62e03abef7a2930
HogHordGit <hoghord@gmail.com>
2022-12-17 16:40:49 +0200

    fix bug with menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,10 +15,35 @@ for (let anchor of anchors) {
 }
 
 // Скрипт для меню бургера
+const list = document.querySelector(".menu__list");
+
+const checkWidth = () => {
+    return window.innerWidth;
+};
+const checkParams = (flag) => {
+    if (checkWidth() > 992 && list.style.display == "none" || checkWidth() >= 992 && flag == true) {
+        return "flex";
+    }
+    if (checkWidth() < 992 && list.style.display == "flex" && flag == true) {
+        return "block";
+    }
+    else if (checkWidth() < 992 && list.style.display == "flex") {
+        return "none";
+    }
+};
+
 $(document).ready(function(){
+    let flag = false;
+
+    setInterval(() => {
+        list.style.display = checkParams(flag);
+    }, 100);
+
 	$('.menu__btn').on('click', function(){
+        flag = flag ? false : true;
+
 		$(this).toggleClass('menu__btn--active');
-		$('.menu__list').stop(true, true).slideToggle(300);
+		$('.menu__list').slideToggle(300);
 	});
 });
 

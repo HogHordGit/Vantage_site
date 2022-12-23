9d4913978a1af91a8d8d7629716dbd3d1e8cd2eb
HogHordGit <hoghord@gmail.com>
2022-12-24 00:16:19 +0200

    make low interval anim

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -72,7 +72,7 @@ function onEntry(entry) {
       }
     });
   }
-  let options = { threshold: [0.5] };
+  let options = { threshold: [0.4] };
   let observer = new IntersectionObserver(onEntry, options);
   let elements = document.querySelectorAll('.element-animation');
   for (let elm of elements) {
